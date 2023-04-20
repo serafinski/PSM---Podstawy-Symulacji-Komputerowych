f6c290b36ba9f44da70ea4fb6cac976fdefc0a7a
Tomasz Serafiński <work.serafinski@gmail.com>
2023-04-20 22:33:56 +0200

    finished

--- Cwiczenia_6/main.py
+++ Cwiczenia_6/main.py
@@ -1,69 +1,80 @@
-import math
 import numpy as np
+import matplotlib.pyplot as plt
 
-l = math.pi
-n = 10
-dx = l / n
-dt = 0.2
-x = []
-i = []
-y = []
-v = []
+l = np.pi  # długość obszaru
+n = 10  # liczba punktów siatki
+dx = l / n  # odstęp między punktami siatki
+dt = 0.2  # krok czasowy
+liczba_krokow = 50  # liczba kroków czasowych
 
-ep = []
+# inicjalizacja pozycji
+pozycje = np.zeros(n + 1)
+for i in range(n + 1):
+    pozycje[i] = np.sin(i * dx)
+
+# inicjalizacja prędkości
+predkosci = np.zeros(n + 1)
+
+# listy energii
 ek = []
+ep = []
+ec = []
 
 
-def init_i():
-    for var in range(11):
-        i.append(var)
+# Funkcja wyliczająca przyspieszenie na podstawie pozycji
+def wylicz_przyspieszenie(pozycje, dx):
+    przyspieszenie = np.zeros(n + 1)
+    for i in range(1, n):
+        przyspieszenie[i] = (pozycje[i - 1] - 2 * pozycje[i] + pozycje[i + 1]) / dx ** 2
+    return przyspieszenie
 
 
-def init_x():
-    for var in i:
-        tmp = dx * var
-        x.append(tmp)
+# Funkcja wykonująca jeden krok symulacji
+def krok(pozycje, predkosci, przyspieszenia, dt, dx):
+    polowa_predkosci = np.zeros(n + 1)
+    for i in range(n + 1):
+        polowa_predkosci[i] = predkosci[i] + 0.5 * przyspieszenia[i] * dt
+    nowe_pozycje = np.zeros(n + 1)
+    for i in range(n + 1):
+        nowe_pozycje[i] = pozycje[i] + polowa_predkosci[i] * dt
+    nowe_przyspieszenia = wylicz_przyspieszenie(nowe_pozycje, dx)
+    nowe_predkosci = np.zeros(n + 1)
+    for i in range(n + 1):
+        nowe_predkosci[i] = polowa_predkosci[i] + 0.5 * nowe_przyspieszenia[i] * dt
+    return nowe_pozycje, nowe_predkosci, nowe_przyspieszenia
 
 
-def init_y():
-    for var in x:
-        tmp = math.sin(var) / 1000
-        y.append(tmp)
+# Funkcja wyliczająca energie na podstawie pozycji i prędkości
+def wylicz_energie(pozycje, przyspieszenia, dx):
+    ek = 0.0
+    ep = 0.0
+    for i in range(1, n):
+        ek += 0.5 * dx * przyspieszenia[i] ** 2
+        ep += 0.5 * (pozycje[i + 1] - pozycje[i]) ** 2 / dx
+    return ek, ep
 
 
-def init_v():
-    tmp = np.zeros(10)
-    v.extend(tmp)
+przyspieszenie = wylicz_przyspieszenie(pozycje, dx)
 
+# SYMULACJA
+for i in range(liczba_krokow):
+    pozycje, predkosci, przyspieszenie = krok(pozycje, predkosci, przyspieszenie, dt, dx)
+    en_kin, en_pot = wylicz_energie(pozycje, predkosci, dx)
+    en_cal = en_kin + en_pot
 
-def count_ep(array):
-    array = np.array(array)
-    tmp = 0
-    for var in range(1, len(array)):
-        tmp += (array[var] - array[var - 1]) ** 2
+    ek.append(en_kin)
+    ep.append(en_pot)
+    ec.append(en_cal)
 
-    final = 1 / (2 * dx) * tmp
+print(ek)
+print(ep)
+print(ec)
 
-    ep.append(final)
-
-
-def count_ek(array):
-    array = np.array(array)
-    tmp = 0
-    for var in range(1, len(array)):
-        tmp += array[var] ** 2
-
-    final = dx / 2 * tmp
-
-    ek.append(final)
-
-
-if __name__ == "__main__":
-    init_i()
-    init_x()
-    init_y()
-    init_v()
-
-    count_ep(y)
-    count_ek(v)
-    print(ek)
+# WYKRES
+plt.plot(ek, label='Energia Kinetyczna')
+plt.plot(ep, label='Energia Potencjalna')
+plt.plot(ec, label='Energia całkowita')
+plt.legend()
+plt.xlabel('Czas (w krokach)')
+plt.ylabel('Energia')
+plt.show()
